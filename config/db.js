8c38a5a304e7279582694f3da676fb25c06d2aae
Charitha-Reddy1 <carg2800@gmail.com>
2026-05-25 13:31:05 +0530

    db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,17 @@
 import mongoose from "mongoose";
+
 const dbConnect = async () => {
   try {
+
     await mongoose.connect(process.env.MONGO_URI);
-  } catch (err) {
-    console.log(err);
+
+    console.log("MongoDB Connected");
+
+  } catch (error) {
+
+    console.log("MongoDB Error:", error);
+
   }
 };
+
 export default dbConnect;
